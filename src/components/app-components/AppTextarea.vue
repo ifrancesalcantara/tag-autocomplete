72464a8eaa8f1b16776ce7d7bbfff2576f3fcc8a
Iván Francés <ivan.frances@easygoband.com>
2021-10-13 01:22:54 +0200

    allow tag add by click

--- src/components/app-components/AppTextarea.vue
+++ src/components/app-components/AppTextarea.vue
@@ -19,7 +19,7 @@
     </mentionable>
     <v-menu v-if="loading || filteredTags.length" :value="!!addingTagText" :position-x="positionX" :nudge-bottom="nudgeBottom" :min-width="xsOnly ? '95%' : '300px'">
       <v-list class="pt-1 pb-0" :class="{ 'min-height-100px': loading }">
-        <v-list-item v-for="(tag, i) in filteredTags" :key="i" @click="$refs.mentionable.applyMention(i)" :class="{ 'active-tag': $refs.mentionable.selectedIndex === i }">
+        <v-list-item v-for="(tag, i) in filteredTags" :key="i" @click="applyMentionByClick(i)" :class="{ 'active-tag': $refs.mentionable.selectedIndex === i }">
           <v-list-item-title>{{ tag.label }}</v-list-item-title>
         </v-list-item>
       </v-list>
@@ -59,19 +59,21 @@ export default {
     }
   },
   methods: {
-    getInnerHTML () {
+    getInnerHTML (childNumOffset) {
       this.addingTagText = ''
-      const html = this.textValue.split('\n').map((p) => {
-        return p.split(' ').map((w) => {
-          return w.startsWith('#') && w !== '#'
-            ? `<span class="active-tag" contentEditable="false">${w.trim()}</span>`
-            : w.trim()
-          }).join(' ')
+      const html = this.textValue
+        .replace('[object Object]', '#') // TODO fix item click adding '[object Object]' instead of hash
+        .split('\n').map((p) => {
+          return p.split(' ').map((w) => {
+            return w.startsWith('#') && w !== '#'
+              ? `<span class="active-tag" contentEditable="false">${w.trim()}</span>`
+              : w.trim()
+            }).join(' ')
       })
-        .filter((p, i) => p || this.textValue.split('\n')[i + 1])
+        .filter((p, i) => p || this.textValue.split('\n')[i + 1]) // TODO fix: allow multiple newlines
         .join('\n')
       this.$refs[this.elementId].innerHTML = html
-      setCaretPosition({ el: this.$refs[this.elementId], childNum: this.$refs[this.elementId].childNodes.length - 1, charNum: 1 })
+      setCaretPosition({ el: this.$refs[this.elementId], childNum: this.$refs[this.elementId].childNodes.length - 1 + (childNumOffset || 0), charNum: 1 })
     },
     listTags (search) {
       const { top, height, left } = this.$refs[this.elementId].getBoundingClientRect()
@@ -89,13 +91,22 @@ export default {
     },
     onKeyDown (e) {
       if (e.key === 'Enter' && this.$refs.mentionable.selectedIndex && this.filteredTags.length) {
-        e.preventDefault()
-        this.getInnerHTML()
         this.$refs.mentionable.applyMention(this.$refs.mentionable.selectedIndex)
+        this.getInnerHTML()
+        this.filteredTags = []
       } else if (e.key === ' ' && this.addingTagText) {
         e.preventDefault()
-        this.getInnerHTML()
+        this.getInnerHTML(-1)
+      } else {
       }
+    },
+    applyMentionByClick (i) {
+      this.$nextTick(() => {
+        const m = this.$refs.mentionable
+        m.openMenu(m.getLastKeyBeforeCaret(m.getSelectionStart()))
+        m.selectedIndex = i
+        m.applyMention(i)
+      })
     }
   },
   watch: {
